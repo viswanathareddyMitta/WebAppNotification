845a21ff39a18272be9a7ea53414b3d562b7c235
viswanathareddy.m <viswanathareddy.m@servion.com>
2025-04-30 17:49:20 +0530

    changed  Stage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace NotifierWeb.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page   .";
 
             return View();
         }
